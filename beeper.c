5e03aebc2b774b199478a5f30ce98a5554f15cc1
lalaithion <izaak.weiss@gmail.com>
2016-01-27 15:18:30 -0700

    beeper added

---
+++ beeper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	return 0;
+}
