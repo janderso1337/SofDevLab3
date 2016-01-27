72244c61f75d37075219cd5c9b61e4efba8bd259
janderso1337 <josh.anderson1219@gmail.com>
2016-01-27 15:51:05 -0700

    wat

--- beeper.c
+++ beeper.c
@@ -2,10 +2,40 @@
 #include <unistd.h>
 int main(void)
 {
+<<<<<<< Updated upstream
 	printf("\a");;;\
 	printf("\a");;;;
 	printf("\a");;;
 	printf("\a");;;
+=======
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	printf("I am making changes"
+>>>>>>> Stashed changes
 	printf("\a");
 	printf("\a");
 	printf("\a");;;;;
